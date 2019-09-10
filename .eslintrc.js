b4862bf1f1158f6c5199d382d5d9c2d4bd26457b
Kris Chambers <kris.chambers@outlook.com>
2019-09-10 13:30:32 -0500

    Made some modifications so we have a project launch / settings being shared. This might be more appropriate in a workspace.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,38 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    sourceType: "module",
-    project: "tsconfig.develop.json"
-  },
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: ["plugin:@typescript-eslint/recommended"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly"
+	},
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		sourceType: "module",
+		project: "tsconfig.develop.json"
+	},
 
-  rules: {
-    "brace-style": ["warn", "allman"],
-    indent: "off",
-    "@typescript-eslint/indent": ["warn", "tab"],
-    "no-trailing-spaces": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "linebreak-style": ["warn", "windows"],
-    quotes: ["warn", "double"],
-    semi: ["warn", "never"]
-  },
-  overrides: [
-    {
-      // Rules for tests.
-      files: ["Source/**/*.spec.ts"],
-      rules: {
-        "no-undef": "off"
-      }
-    }
-  ]
+	rules: {
+		"brace-style": ["warn", "allman"],
+		indent: "off",
+		"@typescript-eslint/indent": ["warn", "tab"],
+		"no-trailing-spaces": "warn",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-member-accessibility": "off",
+		"linebreak-style": ["warn", "windows"],
+		quotes: ["warn", "double"],
+		semi: ["warn", "never"]
+	},
+	overrides: [
+		{
+			// Rules for tests.
+			files: ["Source/**/*.spec.ts"],
+			rules: {
+				"no-undef": "off"
+			}
+		}
+	]
 };
